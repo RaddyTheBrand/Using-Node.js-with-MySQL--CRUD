d7c4fc24c3fe906c6eb1f36321190948542f2f08
Raddy <rx3mer@live.co.uk>
2020-10-18 19:26:49 +0100

    Project

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# Using-Node.js-with-MySQL--CRUD
+# Using Node.js with MySQL – CRUD | XAMPP / PhpMyAdmin
+
+This was created as part of YouTube Video. Links below
+
+![GitHub Logo](https://raddy.co.uk/wp-content/uploads/2020/10/Using-Node.js-with-MySQL-CRUD-XAMPP-PhpMyAdmin_compressed.jpg)
+
+## Installation
+To run this project, install it locally using npm:
+
+```
+$ npm install
+$ npm start
+```
+
+### YouTube Video & Article
+
+[Node.js News Website](https://youtu.be/EkQc-8uzxIA)
+
+[Read Article](https://raddy.co.uk/blog/build-news-website-with-node-js-express-ejs-wp-rest-api/)
+
+### YouTube Channel - RaddyTheBrand
+
+[Subscribe to my YouTube Channel](https://www.youtube.com/channel/UCvXscyQ0cLzPZeNOeXI45Sw?sub_confirmation=1)
+
+### Website
+[www.raddy.co.uk](https://www.raddy.co.uk)
+
+### Donations
+[Via Paypal](https://www.paypal.me/RadoslavAngelov)
+
+[Buy me a Coffee](https://www.buymeacoffee.com/RaddyTheBrand)
---
+++ app.js
@@ -0,0 +1,135 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mysql = require('mysql')
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+
+// MySQL
+const pool = mysql.createPool({
+    connectionLimit : 10,
+    host            : 'localhost',
+    user            : 'root',
+    password        : 'password',
+    database        : 'nodejs_beers'
+})
+
+// Get all beers
+app.get('', (req, res) => {
+
+    pool.getConnection((err, connection) => {
+        if(err) throw err
+        console.log(`connected as id ${connection.threadId}`)
+
+        connection.query('SELECT * from beers', (err, rows) => {
+            connection.release() // return the connection to pool
+
+            if(!err) {
+                res.send(rows)
+            } else {
+                console.log(err)
+            }
+
+        })
+    })
+})
+
+
+// Get a beer by ID
+app.get('/:id', (req, res) => {
+
+    pool.getConnection((err, connection) => {
+        if(err) throw err
+        console.log(`connected as id ${connection.threadId}`)
+
+        connection.query('SELECT * from beers WHERE id = ?', [req.params.id], (err, rows) => {
+            connection.release() // return the connection to pool
+
+            if(!err) {
+                res.send(rows)
+            } else {
+                console.log(err)
+            }
+
+        })
+    })
+})
+
+// Delete a records / beer
+app.delete('/:id', (req, res) => {
+
+    pool.getConnection((err, connection) => {
+        if(err) throw err
+        console.log(`connected as id ${connection.threadId}`)
+
+        connection.query('DELETE from beers WHERE id = ?', [req.params.id], (err, rows) => {
+            connection.release() // return the connection to pool
+
+            if(!err) {
+                res.send(`Beer with the Record ID: ${[req.params.id]} has been removed.`)
+            } else {
+                console.log(err)
+            }
+
+        })
+    })
+})
+
+
+// Add a record / beer
+app.post('', (req, res) => {
+
+    pool.getConnection((err, connection) => {
+        if(err) throw err
+        console.log(`connected as id ${connection.threadId}`)
+
+        const params = req.body
+
+        connection.query('INSERT INTO beers SET ?', params , (err, rows) => {
+            connection.release() // return the connection to pool
+
+            if(!err) {
+                res.send(`Beer with the name: ${params.name} has been added.`)
+            } else {
+                console.log(err)
+            }
+
+        })
+
+        console.log(req.body)
+    })
+})
+
+
+// Update a record / beer
+app.put('', (req, res) => {
+
+    pool.getConnection((err, connection) => {
+        if(err) throw err
+        console.log(`connected as id ${connection.threadId}`)
+
+        const { id, name, tagline, description, image } = req.body
+
+        connection.query('UPDATE beers SET name = ?, tagline = ?, description = ?, image = ? WHERE id = ?', [name, tagline, description, image, id] , (err, rows) => {
+            connection.release() // return the connection to pool
+
+            if(!err) {
+                res.send(`Beer with the name: ${name} has been added.`)
+            } else {
+                console.log(err)
+            }
+
+        })
+
+        console.log(req.body)
+    })
+})
+
+
+
+// Listen on enviroment port or 5000
+app.listen(port, () => console.log(`Listen on port ${port}`))
